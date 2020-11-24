c51ebbe8771aea602fb218da72273d6037e8b900
yuriy-dezhniuk <dezhniuk23@gmail.com>
2020-11-24 22:52:21 +0200

    form fields

---
+++ src/views/Signup.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app>
+    <v-container bg fill-height grid-list-md text-xs-center>
+      <v-layout row wrap align-center>
+        <v-flex>
+          <v-row justify="center">
+            <v-col
+              cols="12"
+              sm="7"
+              md="6"
+              lg="5"
+              xl="3"
+            >
+              <!-- <v-form
+                ref="form"
+                v-model="valid"
+                lazy-validation
+              > -->
+              <v-form
+                color="red"
+                ref="form"
+                lazy-validation
+              >
+                <!-- <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  label="Mail*"
+                  required
+                ></v-text-field> -->
+                <v-text-field
+                  label="E-mail*"
+                  required
+                ></v-text-field>
+
+                <v-text-field
+                  type="password"
+                  required
+                  name="input-10-1"
+                  label="Password*"
+                  hint="At least 8 characters"
+                  counter
+                ></v-text-field>
+
+                <v-text-field
+                  type="password"
+                  required
+                  name="input-10-1"
+                  label="Confirm Password*"
+                  hint="At least 8 characters"
+                  counter
+                ></v-text-field>
+                <v-btn class="mt-4">SIGN UP</v-btn>
+              </v-form>
+            </v-col>
+          </v-row>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'Signup',
+};
+</script>
