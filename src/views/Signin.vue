d9fd067e9a68f105fba1610bb9937c43168e1272
yuriy-dezhniuk <33997334+yuriy-dezhniuk@users.noreply.github.com>
2020-11-27 07:41:58 +0200

    sign up

---
+++ src/views/Signin.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+                <h1 class="font-weight-light text-center pt-4">Sign In</h1>
+                <p class="text-center">Don’t have an account, please
+                  <router-link to="/signup">Sign Up</router-link>
+                </p>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="E-mail*"
+                    required
+                    class="ma-2"
+                    color="dark lighten-1"
+                    v-model="userEmail"
+                  ></v-text-field>
+                  <v-text-field
+                    id="password"
+                    name="password"
+                    label="Password*"
+                    type="password"
+                    class="ma-2"
+                    color="dark lighten-1"
+                    v-model="userPassword"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions class="d-flex pb-6">
+                <v-spacer></v-spacer>
+                <v-btn
+                  type="submit"
+                  color="dark lighten-1"
+                  @click="signIn"
+                >Sign In</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import emailRegExp from '../helpers/emailRegExp.vue';
+
+export default {
+  name: 'Signup',
+  data: () => ({
+    userEmail: '',
+    userPassword: '',
+  }),
+  methods: {
+    signIn() {
+      const userEmail = this.userEmail.trim();
+      if (!emailRegExp.test(userEmail)) {
+        alert('Email address is invalid');
+      } else if (!this.userPassword) {
+        alert('Fields cannot be empty');
+      } else {
+        alert('Sign In is successful');
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/Signup.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+                <h1 class="font-weight-light text-center pt-4">Sign Up</h1>
+                <p class="text-center">Already have an account, please
+                  <router-link to="/signin">Sign In</router-link>
+                </p>
+              <v-card-text>
+                <v-form>
+                  <!-- prepend-icon="mdi-email" -->
+                  <v-text-field
+                    name="email"
+                    label="E-mail*"
+                    color="dark lighten-1"
+                    class="ma-2"
+                    type="text"
+                    v-model="userEmail"
+                  ></v-text-field>
+                  <v-text-field
+                    id="password"
+                    name="password"
+                    label="Password*"
+                    type="password"
+                    class="ma-2"
+                    color="dark lighten-1"
+                    v-model="userPassword"
+                  ></v-text-field>
+                  <v-text-field
+                    id="confirm-password"
+                    name="confirm-password"
+                    label="Confirm Password*"
+                    type="password"
+                    class="ma-2"
+                    color="dark lighten-1"
+                    v-model="userPasswordConfirm"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions class="d-flex pb-6">
+                <v-spacer></v-spacer>
+                <v-btn
+                  type="submit"
+                  color="dark lighten-1"
+                  @click="signUp"
+                >Sign Up</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import emailRegExp from '../helpers/emailRegExp.vue';
+
+export default {
+  name: 'Signup',
+  data: () => ({
+    userEmail: '',
+    userPassword: '',
+    userPasswordConfirm: '',
+  }),
+  methods: {
+    signUp() {
+      if (!emailRegExp.test(this.userEmail)) {
+        alert('Email address is invalid');
+      } else if (!this.userPassword.trim()) {
+        alert('Fields cannot be empty');
+      } else if (this.userPassword !== this.userPasswordConfirm) {
+        alert('Passwords do not match');
+      } else {
+        alert('Sign Up is successful');
+      }
+    },
+  },
+};
+</script>
